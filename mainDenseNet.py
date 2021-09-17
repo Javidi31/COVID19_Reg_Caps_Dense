ef54ccd7dd82e4185e591354a4c7cd89fc51cee7
Malihe Javidi <55978351+Javidi31@users.noreply.github.com>
2021-09-17 14:36:12 +0430

    Update mainDenseNet.py

--- mainDenseNet.py
+++ mainDenseNet.py
@@ -18,7 +18,6 @@ from keras.callbacks import ReduceLROnPlateau, CSVLogger, EarlyStopping
 from keras.utils import np_utils
 from sklearn.model_selection import KFold
 from keras.callbacks import ModelCheckpoint
-# We only test DenseNet-121 in this script for demo purpose
 from densenet169 import DenseNet 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
@@ -27,7 +26,6 @@ from sklearn.metrics import classification_report
 
 lr_reducer    = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6)
 early_stopper = EarlyStopping(min_delta=0.001, patience=100)
-#csv_logger    = CSVLogger('DenseNet169_COVID_KFOLD1.csv')
 
 
 batch_size    = 8
@@ -35,11 +33,10 @@ nb_epoch      = 100
 data_augmentation = False
 img_rows, img_cols = 224, 224
 img_channels  = 1
-#validation_split = 0.05
 num_folds = 5
 
 #LOAD Train data##############################################################################
-path1        = 'D:\\DeepLearning\\COVID19\\Dataset\\My_COVID_CT\p3\\'
+path1        = '.\\Dataset\\COVID_CT\\'
 all_images   = []
 targets      = []
 subjects     = os.listdir(path1)
@@ -51,7 +48,7 @@ for number1 in range(0, num_classes):
     path2 = (path1 + subjects[number1] + '\\')
     files = os.listdir(path2);
     numberOfsamples = len(files)
-    for number2 in range(0,numberOfsamples):#(0,train_no):
+    for number2 in range(0,numberOfsamples):
         path3 = path2 + files[number2]
         img = cv2.imread(path3 , 0)
         img = cv2.resize(img,(img_rows,img_rows))
@@ -61,10 +58,7 @@ for number1 in range(0, num_classes):
 
 inputs     = np.array(all_images)
 inputs     = inputs.astype('float32')
-#mean_image = np.mean(inputs, axis=0)
-#std_image  = np.std(inputs, axis=0)
-#inputs    -= mean_image
-#inputs    *= std_image
+
 mean_score = np.mean(inputs)
 std_score  = np.std(inputs)
 inputs    -= mean_score
@@ -91,18 +85,13 @@ for train, test in kfold.split(inputs, targets):
     str_logger='DenseNet169_COVID_KFOLD'+str(fold_no)+'.csv'
     csv_logger    = CSVLogger(str_logger)
     
-    #model = DenseNet(img_rows=224, img_cols=224, reduction=0.5, classes=num_classes, weights_path=weights_path)
     model = DenseNet(img_rows, img_cols,  reduction=0.5, classes=num_classes)    
-    #MALIHE COMMENT THIS
-    #sgd   = SGD(lr=1e-3, decay=1e-4, momentum=0.9, nesterov=True)#lr=1e-2 decay=1e-6 momentum=0.9, nesterov=True
-    #model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
-    #End MALIHE COMMENT THIS
     opt = keras.optimizers.adam(lr=0.0001)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
     #model.summary()
         
     #Train model with our data
-    MODEL_DIR = "D:\\DeepLearning\\COVID19\\PythonCode\\DenseNet-Keras-master\\logsFolder\\denseNet_CTCOVID_KFOLD1_ranSeed"                         # ********
+    MODEL_DIR = ".\\DenseNetModels\\COVID_CT"                         # ********
     filepath = "saved-model-{epoch:02d}-{val_acc:.4f}.hdf5"
 
     checkpoint = ModelCheckpoint(filepath=os.path.join(MODEL_DIR,
@@ -148,29 +137,3 @@ print('Average scores for all folds:')
 print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
 print(f'> Loss: {np.mean(loss_per_fold)}')
 print('------------------------------------------------------------------------')
-
-
-#f = open('InputTest.pckl', 'wb')
-#pickle.dump( inputs[test], f)
-#f.close()
-#f = open('InputTrain.pckl', 'wb')
-#pickle.dump( inputs[train], f)
-#f.close()
-#f = open('TargetTest.pckl', 'wb')
-#pickle.dump( targets[test], f)
-#f.close()
-#f = open('TargetTrain.pckl', 'wb')
-#pickle.dump( targets[train], f)
-#f.close()
-#
-#f = open('IndTrain.pckl', 'wb')
-#pickle.dump( train, f)
-#f.close()
-#f = open('IndTest.pckl', 'wb')
-#pickle.dump( test, f)
-#f.close()
-
-#load variables
-#f = open('TrLCedar.pckl', 'rb')
-#y_train = pickle.load(f)
-#f.close()
